21c4374d2f70f8b9673ca63e0ca2933e17cdf4bd
balticoceanblue <robert.w.ballard@gmail.com>
2017-11-07 18:29:31 -0800

    blocChat checkpoint 2 list rooms, completed

---
+++ app/scripts/services/Room.js
@@ -0,0 +1,13 @@
+(function() {
+  function Room($firebaseArray) {
+    var ref = firebase.database().ref().child("rooms");
+    var rooms = $firebaseArray(ref);
+    return {
+      all: rooms
+    };
+  }
+
+  angular
+  .module('blocChat')
+  .factory('Room', ['$firebaseArray', Room]);
+})();

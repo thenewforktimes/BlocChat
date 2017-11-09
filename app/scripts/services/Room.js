0a319e6798b88ae16abe7d2ed846c2fffa6ff72d
balticoceanblue <robert.w.ballard@gmail.com>
2017-11-09 09:45:53 -0800

    checkpoint 3 create rooms completed

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,10 +1,17 @@
 (function() {
   function Room($firebaseArray) {
+    var Room = {};
+
     var ref = firebase.database().ref().child("rooms");
     var rooms = $firebaseArray(ref);
-    return {
-      all: rooms
+
+    Room.all = rooms;
+
+    Room.add = function (room) {
+      rooms.$add(room);
     };
+
+    return Room;
   }
 
   angular
